40cf716ef23b87dec5f59de04ab75c638bc379c3
Nick Stevens <nick@nickstevens.com>
2014-01-24 17:22:45 -0700

    Styles updates. Ads. JS.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,63 @@
+(function() {
+	$(".ad").fancybox({
+		type: 'iframe',
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
 
+	$(".related-content a").fancybox({
+		type: 'iframe',
+		maxWidth	: 800,
+		maxHeight	: 800,
+		fitToView	: false,
+		width		: '70%',
+		height		: '80%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+
+	$('#search')
+        .textext({
+            plugins : 'autocomplete'
+        })
+        .bind('getSuggestions', function(e, data) {
+        var list = [
+			'Verizon customer service',
+			'Comcast bundles',
+			'Chevy deal days',
+			'Volkswagen sign and drive',
+			'Coke Rewards',
+			'Pepsi Cindy Crawford commercial',
+			'McDonald&#39;s breakfast menu',
+			'Apple store',
+			'Cheerios commercial',
+			'AT&T coverage area',
+			'The Oscars Awards 2014',
+			'Subway Jared',
+			'Amazon.com promo codes',
+			'Android phones',
+			'History Channel shows',
+			'Obamacare website',
+			'Monster.com login',
+			'Mastercard credit card offers',
+			'Marlboro healthy cigarettes',
+			'Louis Vuitton collections'
+        ],
+        textext = $(e.target).textext()[0],
+        query = (data ? data.query : '') || '';
+
+        $(this).trigger(
+            'setSuggestions',
+            { result : textext.itemManager().filter(list, query) }
+        );
+    });
+})();
